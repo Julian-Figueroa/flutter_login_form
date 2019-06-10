7ced040292ad98c02fa34fbb13c76f1615e1887d
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-06-10 08:07:49 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login' : (BuildContext context) => LoginPage(),
+        'home' : (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _createBackground(context),
+          _loginForm(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _createBackground(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final purpleBackground = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[
+            Color.fromRGBO(63, 63, 156, 1.0),
+            Color.fromRGBO(90, 70, 178, 1.0),
+          ],
+        ),
+      ),
+    );
+
+    final circle = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05),
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        purpleBackground,
+        Positioned(
+          top: 90.0,
+          left: 30.0,
+          child: circle,
+        ),
+        Positioned(
+          top: -40.0,
+          right: -30.0,
+          child: circle,
+        ),
+        Positioned(
+          bottom: -50.0,
+          right: -10.0,
+          child: circle,
+        ),
+        Positioned(
+          bottom: 120.0,
+          right: 20.0,
+          child: circle,
+        ),
+        Positioned(
+          bottom: -50.0,
+          left: -20.0,
+          child: circle,
+        ),
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(
+                Icons.person_pin_circle,
+                color: Colors.white,
+                size: 100.0,
+              ),
+              SizedBox(
+                height: 10.0,
+                width: double.infinity,
+              ),
+              Text(
+                'Julian Figueroa',
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric(
+              vertical: 50.0,
+            ),
+            margin: EdgeInsets.symmetric(
+              vertical: 30.0,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                    spreadRadius: 3.0,
+                  ),
+                ]),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Sign up',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 60.0,
+                ),
+                _createEmail(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _createPassword(),
+                SizedBox(
+                  height: 60.0,
+                ),
+                _createButton(),
+              ],
+            ),
+          ),
+          Text('Forgot password ?'),
+          SizedBox(
+            height: 100.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20.0,
+      ),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(
+              Icons.alternate_email,
+              color: Colors.deepPurple,
+            ),
+            hintText: 'example@email.com',
+            labelText: 'E - mail'),
+      ),
+    );
+  }
+
+  Widget _createPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20.0,
+      ),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.vpn_key,
+            color: Colors.deepPurple,
+          ),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+
+  Widget _createButton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 80.0,
+          vertical: 15.0,
+        ),
+        child: Text('Login'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: () {},
+    );
+  }
+}
